43928e05e3f0427fd382c4b8299dc1e155935042
sato1214syun <sato1214syun@gmail.com>
2026-01-14 08:51:23 +0900

    8.1完了 リファクタリングbyruff

--- script/func.py
+++ script/func.py
@@ -1,4 +1,7 @@
+"""補助関数."""
+
 import math
+from itertools import combinations
 from pathlib import Path
 from typing import Literal, overload
 
@@ -30,7 +33,7 @@ from sklearn.svm import OneClassSVM
 
 
 # カーネル 11 種類
-def generate_kernels(x: np.ndarray | pl.DataFrame) -> list[Sum]:
+def generate_kernels(x: np.ndarray | pl.DataFrame) -> list[Sum]:  # noqa: D103
     return [
         ConstantKernel() * DotProduct() + WhiteKernel(),
         ConstantKernel() * RBF() + WhiteKernel(),
@@ -76,26 +79,11 @@ def load_data(file_path, index=None):
     return x, y
 
 
-def save_csv(
-    data: pl.DataFrame,
-    file_path: Path | str,
-    add_row_index: bool = False,
-    index_name: str = "",
-    offset: int = 0,
-    quote_style: str = "never",
-) -> None:
-    """Save DataFrame to a CSV file."""
-    if add_row_index:
-        data = data.clone().with_row_index(name=index_name, offset=offset)
-    data.write_csv(file_path, quote_style=quote_style)
-
-
-def delete_zero_std_columns(data: pl.DataFrame) -> pl.DataFrame:
+def delete_zero_std_columns(data: pl.DataFrame) -> pl.DataFrame:  # noqa: D103
     zero_stdev_cols = [
         col for col, std in data.std().row(0, named=True).items() if std in [0, None]
     ]
-    data_wo_zero_std = data.drop(zero_stdev_cols)
-    return data_wo_zero_std
+    return data.drop(zero_stdev_cols)
 
 
 @overload
@@ -108,43 +96,43 @@ def autoscaling(
 def autoscaling(data: pl.Series, data2: pl.Series | None = None) -> pl.Series: ...
 
 
-def autoscaling(data, data2=None):
+def autoscaling(data, data2=None):  # noqa: D103
     if data2 is None:
         if isinstance(data, pl.Series):
             return (data - data.mean()) / data.std()
-        elif isinstance(data, pl.DataFrame):
+        if isinstance(data, pl.DataFrame):
             return data.select((pl.all() - pl.all().mean()) / pl.all().std())
-        else:
-            raise TypeError("Input must be a Polars Series or DataFrame.")
+        msg = "Input must be a Polars Series or DataFrame."
+        raise TypeError(msg)
 
     if isinstance(data, pl.Series) and isinstance(data2, pl.Series):
         return (data - data2.mean()) / data2.std()
-    elif isinstance(data, pl.DataFrame) and isinstance(data2, pl.DataFrame):
+    if isinstance(data, pl.DataFrame) and isinstance(data2, pl.DataFrame):
         return (
             data - data2.mean().select(pl.all().repeat_by(data.height).explode())
         ) / data2.std().select(pl.all().repeat_by(data.height).explode())
-    else:
-        raise TypeError("Input must be a Polars Series or DataFrame.")
+    msg = "Input must be a Polars Series or DataFrame."
+    raise TypeError(msg)
 
 
 @overload
 def rescaling(
-    autoscaled_data: pl.DataFrame, original_data: pl.DataFrame, is_std: bool = False
+    autoscaled_data: pl.DataFrame, original_data: pl.DataFrame, *, is_std: bool = False
 ) -> pl.DataFrame: ...
 
 
 @overload
 def rescaling(
-    autoscaled_data: pl.Series, original_data: pl.Series, is_std: bool = False
+    autoscaled_data: pl.Series, original_data: pl.Series, *, is_std: bool = False
 ) -> pl.Series: ...
 
 
-def rescaling(autoscaled_data, original_data, is_std=False):
+def rescaling(autoscaled_data, original_data, *, is_std=False):  # noqa: D103
     if isinstance(autoscaled_data, pl.Series) and isinstance(original_data, pl.Series):
         if is_std:
             return autoscaled_data * original_data.std()
         return autoscaled_data * original_data.std() + original_data.mean()
-    elif isinstance(autoscaled_data, pl.DataFrame) and isinstance(
+    if isinstance(autoscaled_data, pl.DataFrame) and isinstance(
         original_data, pl.DataFrame
     ):
         if is_std:
@@ -156,8 +144,8 @@ def rescaling(autoscaled_data, original_data, is_std=False):
         ) + original_data.mean().select(
             pl.all().repeat_by(autoscaled_data.height).explode()
         )
-    else:
-        raise TypeError("Input must be a Polars Series or DataFrame.")
+    msg = "Input must be a Polars Series or DataFrame."
+    raise TypeError(msg)
 
 
 def scatter_plot_of_result(
@@ -167,7 +155,7 @@ def scatter_plot_of_result(
     x_label: str = "x",
     y_label: str = "y",
 ) -> None:
-    """実測値 vs. 推定値の散布図を作成して保存する関数"""
+    """実測値 vs. 推定値の散布図を作成して保存する関数."""
     # 実測値 vs. 推定値のプロット
     ax = sns.scatterplot(x=x, y=y, color="blue")
     # 実測値と推定値の両方の最大値・最小値
@@ -187,21 +175,21 @@ def scatter_plot_of_result(
     plt.show()
 
 
-def show_fitting_evaluation(true_val, estimated_val) -> None:
-    """回帰モデルの適合度を評価する関数"""
+def show_fitting_evaluation(true_val: pl.Series, estimated_val: pl.Series) -> None:
+    """回帰モデルの適合度を評価する関数."""
     # トレーニングデータのr2, RMSE, MAE
     print("r^2:", r2_score(true_val, estimated_val))
     print("RMSE:", root_mean_squared_error(true_val, estimated_val))
     print("MAE:", mean_absolute_error(true_val, estimated_val))
 
 
-def calc_r2(
-    model,
+def calc_r2(  # noqa: D103
+    model,  # noqa: ANN001
     x: pl.DataFrame,
     y: pl.Series,
     cv: KFold = None,
     y_true: pl.Series = None,
-    **model_kwargs,
+    **model_kwargs,  # noqa: ANN003
 ) -> float:
     if cv is None:
         if y_true is not None:
@@ -224,7 +212,7 @@ def calc_r2(
 
 
 def calc_optimal_gamma(x: pl.DataFrame, gammas: pl.Series) -> float:
-    """グラム行列の分散が最大となるγを探索する関数"""
+    """グラム行列の分散が最大となるγを探索する関数."""
     x_wt_var_list = x.select(pl.concat_list(pl.all()).alias("sample"))
     sample_distances = (
         # すべてのサンプルを組み合わせる
@@ -244,8 +232,8 @@ def calc_optimal_gamma(x: pl.DataFrame, gammas: pl.Series) -> float:
     return gammas.item(gram_matrix.list.var().arg_max())
 
 
-def optimize_hyperparameters_by_cv(
-    model,
+def optimize_hyperparameters_by_cv(  # noqa: D103, PLR0913
+    model,  # noqa: ANN001
     x: pl.DataFrame,
     y: pl.Series,
     y_true: pl.Series,
@@ -253,6 +241,7 @@ def optimize_hyperparameters_by_cv(
     epsilons: pl.Series,
     gammas: pl.Series,
     cv: KFold,
+    *,
     verbose: bool = True,
 ) -> tuple[float, float, float]:
     # ハイパーパラメータC, ε, γの最適化
@@ -292,7 +281,7 @@ def optimize_hyperparameters_by_cv(
         ],
     )
     optimal_nonlinear_c: float = cs.item(r2_cvs.arg_max())
-    # CV による γ の最適化
+    # CV による gamma の最適化
     r2_cvs = pl.Series(
         "gammas_r2",
         [
@@ -310,13 +299,16 @@ def optimize_hyperparameters_by_cv(
     # 結果の確認
     if verbose:
         print(
-            f"最適化された C : {optimal_nonlinear_c} (log(C)={math.log2(optimal_nonlinear_c)})"
+            f"最適化された C : {optimal_nonlinear_c} "
+            f"(log(C)={math.log2(optimal_nonlinear_c)})"
         )
         print(
-            f"最適化された ε : {optimal_nonlinear_epsilon} (log(ε)={math.log2(optimal_nonlinear_epsilon)})"
+            f"最適化された ε : {optimal_nonlinear_epsilon} "
+            f"(log(ε)={math.log2(optimal_nonlinear_epsilon)})"
         )
         print(
-            f"最適化された γ : {optimal_nonlinear_gamma} (log(γ)={math.log2(optimal_nonlinear_gamma)})"
+            f"最適化された γ : {optimal_nonlinear_gamma} "  # noqa: RUF001
+            f"(log(γ)={math.log2(optimal_nonlinear_gamma)})"  # noqa: RUF001
         )
     return (
         optimal_nonlinear_c,
@@ -325,13 +317,13 @@ def optimize_hyperparameters_by_cv(
     )
 
 
-def optimize_hyperparameters_by_gs(
-    model,
+def optimize_hyperparameters_by_gs(  # noqa: D103
+    model,  # noqa: ANN001
     x: pl.DataFrame,
     y: pl.Series,
     param_grid: dict,
     cv: KFold,
-    **model_kwargs,
+    **model_kwargs,  # noqa: ANN003
 ) -> dict:
     gs = GridSearchCV(estimator=model(**model_kwargs), param_grid=param_grid, cv=cv)
     gs.fit(x, y)
@@ -341,9 +333,7 @@ def optimize_hyperparameters_by_gs(
 
 
 def add_sqrt_and_interaction_terms(df: pl.DataFrame) -> pl.DataFrame:
-    """説明変数の二乗項や交差項を追加"""
-    from itertools import combinations
-
+    """説明変数の二乗項や交差項を追加."""
     return df.with_columns(  # 二乗項の追加
         [(pl.col(col) ** 2).alias(f"{col}^2") for col in df.columns]
     ).with_columns(  # 交差項の追加
@@ -354,17 +344,18 @@ def add_sqrt_and_interaction_terms(df: pl.DataFrame) -> pl.DataFrame:
     )
 
 
-def calc_ad_by_knn(
+def calc_ad_by_knn(  # noqa: D103
     x: pl.DataFrame,
     n_neighbors: int,
     ad_threshold: float = 0.96,
+    *,
     remove_self: bool = True,
 ) -> tuple[pl.Series, pl.Series, float]:
     ad_model = NearestNeighbors(n_neighbors=n_neighbors, metric="euclidean")
     ad_model.fit(x)
 
     if remove_self:
-        # 1(自分自身) + n_neighbors個のサンプルを抽出
+        # 1(自分自身) + n_neighbors個のサンプルを抽出  # noqa: ERA001
         knn_distance_wt_self, _ = ad_model.kneighbors(x, n_neighbors=1 + n_neighbors)
         # 自分自身との距離を削除
         knn_distance = pl.DataFrame(knn_distance_wt_self).drop(pl.first())
@@ -378,33 +369,38 @@ def calc_ad_by_knn(
     return mean_of_knn_distance, within_ad_flag, ad_threshold
 
 
-def calc_ad_by_ocsvm(
+def calc_ad_by_ocsvm(  # noqa: D103
     ad_model: OneClassSVM,
     x: pl.DataFrame,
+    *,
     train_data: bool = True,
 ) -> tuple[pl.Series, pl.Series]:
-    # トレーニングデータのデータ密度 (f(x) の値)を取得し、ADの中/外を判定(0以上:AD内、0未満:AD外)
+    # トレーニングデータのデータ密度 (f(x) の値)を取得し、
+    # ADの中/外を判定(0以上:AD内、0未満:AD外)
     data_density = pl.Series(ad_model.decision_function(x))
     within_ad_flag = data_density.ge(0)
 
     number_of_outliers = data_density.lt(0).sum()
+    support_vec_ratio= ad_model.support_.size / x.height
+    outlier_ratio_train = number_of_outliers / x.height
+    outlier_ratio_pred = number_of_outliers / x.height
     if train_data:
         print(
             f"\nトレーニングデータにおけるサポートベクター数 :{ad_model.support_.size}"
-            f"\nトレーニングデータにおけるサポートベクターの割合 :{ad_model.support_.size / x.height}"
+            f"\nトレーニングデータにおけるサポートベクターの割合 :{support_vec_ratio}"
             f"\nトレーニングデータにおける外れサンプル数 :{number_of_outliers}"
-            f"\nトレーニングデータにおける外れサンプルの割合 :{number_of_outliers / x.height}"
+            f"\nトレーニングデータにおける外れサンプルの割合 :{outlier_ratio_train}"
         )
     else:
         print(
             f"\n予測用データセットにおける外れサンプル数 :{number_of_outliers}",
-            f"\n予測用データセットにおける外れサンプルの割合 :{number_of_outliers / x.height}",
+            f"\n予測用データセットにおける外れサンプルの割合 :{outlier_ratio_pred}",
         )
 
     return data_density, within_ad_flag
 
 
-def calc_acquisition_func(
+def calc_acquisition_func(  # noqa: D103, PLR0913
     y: pl.Series,
     y_std: pl.Series,
     func: Literal["MI", "EI", "PI", "PTR"],
@@ -412,8 +408,6 @@ def calc_acquisition_func(
     relaxation: float,
     delta: float,
 ) -> tuple[pl.Series, pl.Series]:
-    # 比較用(temp)
-
     # MI で必要な "ばらつき" を 0 で初期化
     cumulative_variance = pl.zeros(y.len(), eager=True)
 
